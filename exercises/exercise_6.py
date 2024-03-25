405d7c4ad7957595dd734696db31c06845e4734d
kw4dw0 <152057939+kw4dw0@users.noreply.github.com>
2024-03-25 12:38:50 +0000

    Update exercise_6.py

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,16 @@
 # Your solution to Exercise 6
 
+x1 = int(input("Enter the x-coord of A: "))
+y1 = int(input("Enter the y-coord of A: "))
+x2 = int(input("Enter the x-coord of B: "))
+y2 = int(input("Enter the y-coord of B: "))
+
+Dist_A = (x1**2 + y1**2)**0.5
+Dist_B = (x2**2 + y2**2)**0.5
+
+if Dist_A > Dist_B:
+    print("A is further from the origin.")
+elif Dist_A == Dist_B:
+    print("A and B are at the same distance from the origin.")
+else:
+    print("B is further from the origin.")
