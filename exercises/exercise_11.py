c44e3318a90a6371f659455168408c214bc3388c
kw4dw0 <152057939+kw4dw0@users.noreply.github.com>
2024-03-29 15:39:00 +0000

    Update exercise_11.py

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,2 +1,15 @@
 # Your solution to Exercise 11
+verif = False
+while not verif:
+  year = int(input("Enter a year: "))
+  if 1900 <= year <= 3000:
+    verif = True
 
+cond1 = (year % 4 == 0)
+cond2 = (year % 100 != 0)
+cond3 = (year % 400 == 0)
+
+if cond1 and cond2 or cond3:
+  print("Leap year.")
+else:
+  print("Ordinary year.")
