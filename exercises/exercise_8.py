233de1481234d6feabb4cc345efc224ba10113da
kw4dw0 <152057939+kw4dw0@users.noreply.github.com>
2024-03-29 15:22:04 +0000

    Update exercise_8.py

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,2 +1,10 @@
 # Your solution to Exercise 8
+verif = False
+while not verif:
+  nums3 = int(input("Enter a 3-digit number: "))
+  if len(str(nums3)) == 3:
+    num = int(input("Enter a single digit: "))
+    if len(str(num)) == 1:
+      verif = True
 
+print((str(num) in str(nums3)))
