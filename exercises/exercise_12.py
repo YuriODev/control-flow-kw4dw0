7df1c23b7161e8e8b4a8f8ca62a04c5369a8c529
kw4dw0 <152057939+kw4dw0@users.noreply.github.com>
2024-03-29 16:17:41 +0000

    Update exercise_12.py

--- exercises/exercise_12.py
+++ exercises/exercise_12.py
@@ -1,2 +1,16 @@
 # Your solution to Exercise 12
+verif = False
+while not verif:
+  nums4 = int(input("Enter a year: "))
+  if len(str(nums4)) == 4:
+    verif = True
 
+list_nums4 = list(str(nums4))
+
+for i in range(len(list_nums4)):
+  if int(list_nums4[i]) % 2 == 0:
+    list_nums4[i] = "*"
+
+str_nums4 = "".join(list_nums4)
+
+print(str_nums4)
